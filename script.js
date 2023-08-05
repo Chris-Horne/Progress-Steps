06460021cd2c85474fc0db9e3400aa3d2f88fe21
CHRIS HORNE <87401567+Chris-Horne@users.noreply.github.com>
2023-08-05 09:51:55 -0700

    Next button function fixed

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ next.addEventListener("click", () => {
 prev.addEventListener("click", () => {
   currentActive--;
 
-  if (currentActive > 1) {
+  if (currentActive < 1) {
     currentActive = 1;
   }
 
@@ -36,9 +36,7 @@ function update() {
 
   const actives = document.querySelectorAll(".active");
 
-  console.log(actives.length / circles.length);
-
-  /* progress.style.width =
-    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
-    */
+  progress.style.width = `${
+    ((actives.length - 1) / (circles.length - 1)) * 100
+  }%`;
 }
